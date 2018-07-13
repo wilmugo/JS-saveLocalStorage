33a5c8a0e318a972a1873ef68acd3f590c2e2677
Wilfredo Murillo Gonzalez <wilmugo@gmail.com>
2018-07-12 21:11:48 -0400

    guardar notas en el localstorage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,32 @@
 // Variables
-const tweetList = document.getElementById('tweet-list');
+const noteList = document.getElementById('note-list');
 
 //Eventos
-eventListener();
 function eventListener() {
   document.querySelector('#form').addEventListener('submit', newTweet);
 }
+eventListener();
 
 //Funciones
 function newTweet(e) {
   e.preventDefault();
 
   //leer el valor del texto en el textarea
-  const tweet = document.getElementById('tweet').value;
+  const note = document.getElementById('note').value;
+  document.getElementById('note').value = '';
 
   //crear boton de eliminar
   const removeBtn = document.createElement('a');
-  removeBtn.classList = 'remove-tweet';
+  removeBtn.classList = 'remove-note';
   removeBtn.textContent = 'X';
 
   //crear elemento li donde va ir el texto del textarea
   const li = document.createElement('li');
-  li.textContent = tweet;
+  li.textContent = note;
 
   //poner el boton de eliminar en el li
   li.appendChild(removeBtn);
 
   //colocar el li en el div tweet-list
-  tweetList.appendChild(li);
+  noteList.appendChild(li);
 }
